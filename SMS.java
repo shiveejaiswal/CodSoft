f328c36d1613d5d9e997fc491904dd2ef1864f6b
SHIVEE JAISWAL <shiveejaiswal25@gmail.com>
2023-10-01 12:30:37 +0530

    Add files via upload

---
+++ SMS.java
@@ -0,0 +1,122 @@
+package JavaInternship;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Student {
+    private String name;
+    private int rollNumber;
+    private String grade;
+
+    public Student(String name, int rollNumber, String grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + " | Roll Number: " + rollNumber + " | Grade: " + grade;
+    }
+}
+
+class StudentManagementSystem {
+    private List<Student> students;
+
+    public StudentManagementSystem() {
+        students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+        System.out.println("Student added successfully!");
+    }
+
+    public void displayAllStudents() {
+        System.out.println("List of All Students:");
+        for (Student student : students) {
+            System.out.println(student);
+        }
+    }
+
+    public Student searchStudentByRollNumber(int rollNumber) {
+        for (Student student : students) {
+            if (student.getRollNumber() == rollNumber) {
+                return student;
+            }
+        }
+        return null;
+    }
+}
+
+public class SMS {
+    public static void main(String[] args) {
+        StudentManagementSystem system = new StudentManagementSystem();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nStudent Management System");
+            System.out.println("1. Add Student");
+            System.out.println("2. Display All Students");
+            System.out.println("3. Search for Student");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter Student Name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter Roll Number: ");
+                    int rollNumber = scanner.nextInt();
+                    scanner.nextLine(); // Consume the newline
+                    System.out.print("Enter Grade: ");
+                    String grade = scanner.nextLine();
+
+                    Student student = new Student(name, rollNumber, grade);
+                    system.addStudent(student);
+                    break;
+                case 2:
+                    system.displayAllStudents();
+                    break;
+                case 3:
+                    System.out.print("Enter Roll Number to Search: ");
+                    int searchRollNumber = scanner.nextInt();
+                    Student foundStudent = system.searchStudentByRollNumber(searchRollNumber);
+                    if (foundStudent != null) {
+                        System.out.println("Student found: " + foundStudent);
+                    } else {
+                        System.out.println("Student not found.");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Exiting the program.");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+}
+
